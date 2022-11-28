de99c0b7be00d951bd188fc7bc76012fea0d7b50
muhammadosmanqureshi <115480972+muhammadosmanqureshi@users.noreply.github.com>
2022-11-28 15:26:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var nAme = document.getElementById('nAme')
+var fName = document.getElementById('fName')
+var email = document.getElementById('email')
+var paswrd = document.getElementById('paswrd')
+var cntct = document.getElementById('cntct')
+var cnic = document.getElementById("identity")
+
+
+function getname(){
+
+console.log(nAme.value)
+console.log(fName.value)
+console.log(email.value)
+console.log(paswrd.value)
+console.log(cntct.value)
+console.log(identity.value)
+
+}
